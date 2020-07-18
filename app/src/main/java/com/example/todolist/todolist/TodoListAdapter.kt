eebc6b5bab5300a5a024986994ad0d3e4d65e304
Chengyuan <1136427613@qq.com>
2020-07-18 14:50:03 +0800

    ues recyclerview to display the notes,use another view card

--- app/src/main/java/com/example/todolist/todolist/TodoListAdapter.kt
+++ app/src/main/java/com/example/todolist/todolist/TodoListAdapter.kt
@@ -1,31 +1,60 @@
 package com.example.todolist.todolist
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.R
-import com.example.todolist.TextItemViewHolder
 import com.example.todolist.database.Note
 
-class TodoListAdapter :RecyclerView.Adapter<TextItemViewHolder>(){
-    var data= listOf<Note>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class TodoListAdapter : ListAdapter<Note, TodoListAdapter.ViewHolder>(NoteDiffCallback()){
 
-    override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        val item = data[position]
-        holder.textView.text=item.title
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater
-            .inflate(R.layout.list_note, parent, false) as TextView
-        return TextItemViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(itemView: View):RecyclerView.ViewHolder(itemView){
+        val title:TextView=itemView.findViewById(R.id.textViewDetailsTitle)
+        val time:TextView=itemView.findViewById(R.id.textViewItemTime)
+
+        fun bind(
+            item: Note
+        ) {
+            val res = itemView.context.resources
+            title.text = item.title
+            time.text = item.creatingtime.toString()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater
+                    .inflate(R.layout.list_note, parent, false)
+                return ViewHolder(view)
+            }
+        }
+    }
+
+    class NoteDiffCallback : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.noteId==newItem.noteId
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem==newItem
+        }
+
     }
 }
--- app/src/main/java/com/example/todolist/todolist/TodoListFragment.kt
+++ app/src/main/java/com/example/todolist/todolist/TodoListFragment.kt
@@ -46,7 +46,7 @@ class TodoListFragment :Fragment() {
 
         viewModel.notes.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data=it
+                adapter.submitList(it)
             }
         })
         binding.buttonAdd.setOnClickListener { v: View ->
